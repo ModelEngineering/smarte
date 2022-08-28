94d64924b0c22f9df14dec6e0122c54b4445e601
Joseph Hellerstein <jlheller@uw.edu>
2022-08-28 07:03:40 -0700

    improved script

--- experiments/count.sh
+++ experiments/count.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Counts the Kbytes in .csv files
-ls -lh *.csv | awk '{print $5}' | sed 's/K//' | paste -sd+ - | sed 's/++/+/g' | sed 's/+$//' | bc
+ls -lh *.csv | awk '{print $5}' | sed 's/K//'  | sed 's/M/000/' | \
+   paste -sd+ - | sed 's/++/+/g' | sed 's/+$//' | bc
