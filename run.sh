17cbbd306cbc05be9a5d982cad4ec8f68b5929d4
Joseph Hellerstein <jlheller@uw.edu>
2022-08-31 23:08:56 -0700

    updates

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ do
     echo "*** New iteration at: `date` ***"
     echo ""
     python smarte/experiment_runner.py  &
-    sleep 7200
+    sleep 1800
     bash kill.sh
 done
--- setup_env.sh
+++ setup_env.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-=======
 #python3 -m venv smt
->>>>>>> main
 pyenv exec python -m venv smt
 source smt/bin/activate
 pip install --upgrade pip
