dca9f068232adf37c5a97595a5eeb40b1bd5c21d
Joseph Hellerstein <jlheller@uw.edu>
2022-09-01 17:31:39 -0700

    updated experiments

--- run.sh
+++ run.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-# Runs the experiments for 30min intervals
+# Runs the experiments, monitoring for failures and restarts
 # Must be in smt virtual environment and have done setup_run.sh
-while :
-do
+DELAY=60
+COUNT=3
+
+function runit () {
     echo ""
     echo "*** New iteration at: `date` ***"
     echo ""
     python smarte/experiment_runner.py  &
-    sleep 7200
-    bash kill.sh
+}
+
+# Monitor and restart if necesary
+while :
+do
+    count=`ps aux | grep "multiprocessing.spawn" | wc | awk '{print $1}'`
+    if (( ${count} < ${COUNT})); then
+        runit
+    fi
+    sleep ${DELAY}
 done
