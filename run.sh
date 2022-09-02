5ea1f1aebcd5c59e850bf4d13b74009e391838dc
Joseph Hellerstein <jlheller@uw.edu>
2022-09-02 10:37:02 -0700

    updated experiments 1

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 # Runs the experiments, monitoring for failures and restarts
 # Must be in smt virtual environment and have done setup_run.sh
 # Monitoring interval
-DELAY=300
+DELAY=7200
 # Fewer than this multiprocessing tasks forces a restart
 COUNT=3
 
@@ -10,16 +10,18 @@ function runit () {
     echo ""
     echo "*** New iteration at: `date` ***"
     echo ""
+    bash kill.sh
     python smarte/experiment_runner.py  &
 }
 
 # Monitor and restart if necesary
 while :
 do
-    count=`ps aux | grep "multiprocessing.spawn" | wc | awk '{print $1}'`
-    if (( ${count} < ${COUNT})); then
-        bash kill.sh
-        runit
-    fi
+    #count=`ps aux | grep "multiprocessing.spawn" | wc | awk '{print $1}'`
+    #if (( ${count} < ${COUNT})); then
+    #    bash kill.sh
+    #    runit
+    #fi
+    runit
     sleep ${DELAY}
 done
