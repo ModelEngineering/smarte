ffb9b9f039ca8a3f64224fc5b6e19587965be234
Joseph Hellerstein <jlheller@uw.edu>
2022-09-17 16:28:42 -0700

    update for refactor

--- kill.sh
+++ kill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Kills the computation
-PID=`ps aux | grep "experiment_runner" | grep -v "grep" \
+PID=`ps aux | grep "workunit_runner" | grep -v "grep" \
       | awk '{print $2}'`
 kill -9 ${PID}
