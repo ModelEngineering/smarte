9d2cc26b4ebed14aecfc34aba771af48cb96ac9e
Joseph Hellerstein <jlheller@uw.edu>
2022-09-18 17:17:58 -0700

    Updated bash scripts

--- run.sh
+++ run.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
-# Runs the experiments, monitoring for failures and restarts
+# Runs the experiments specified in experiments/workunits.txt
 # Must be in smt virtual environment and have done setup_run.sh
-# Monitoring interval
-DELAY=10800
-# Fewer than this multiprocessing tasks forces a restart
-COUNT=3
 
-function runit () {
-    echo ""
-    echo "*** New iteration at: `date` ***"
-    echo ""
-    bash kill.sh
-    python $HOME/home/Technical/smarte/smarte/workunit_runner.py  &
-}
-
-# Monitor and restart if necesary
-while :
-do
-    #count=`ps aux | grep "multiprocessing.spawn" | wc | awk '{print $1}'`
-    #if (( ${count} < ${COUNT})); then
-    #    bash kill.sh
-    #    runit
-    #fi
-    runit
-    sleep ${DELAY}
-done
+for w in `cat experiments/workunits.txt`
+  do
+    python smarte/workunit_runner.py $w &
+  done
--- tools/make_workunits.py
+++ tools/make_workunits.py
@@ -7,15 +7,19 @@ import os
 
 
 workunits = []
-for max_fev in [1000, 10000]:
+for max_fev in [1000]:
     for method in ["differential_evolution", "leastsq"]:
-        for latincube_idx in range(1, 11):
+        for latincube_idx in range(1, 2):
             for noise_mag in [0.1, 0.2]:
-                workunit = smt.Workunit(method=method,
-                      latincube_idx=latincube_idx,
-                      noise_mag=noise_mag,
-                      max_fev=max_fev)
-                workunits.append(str(workunit))
+                for ts_instance in [1, 2]:
+                    workunit = smt.Workunit(
+                          max_fev=max_fev,
+                          method=method,
+                          latincube_idx=latincube_idx,
+                          noise_mag=noise_mag,
+                          ts_instance=ts_instance,
+                          )
+                    workunits.append(str(workunit))
 
 with open(cn.WORKUNITS_FILE, "w") as fd:
     fd.write("\n".join(workunits))
