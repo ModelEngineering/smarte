bd9a5c44792cc91ce4a94f8e157e62588de94079
Joseph Hellerstein <jlheller@uw.edu>
2022-10-03 18:30:11 -0700

    Added Anova and tests

--- smarte/analysis/anova.py
+++ smarte/analysis/anova.py
@@ -16,6 +16,8 @@ from scipy.stats import f
 
 # DataFrame columns
 SSB = "ssb"  # sum of squares
+INSTANCE = "instance"
+SEPARATOR = "__"  # Separates names in instances
 
 
 # stat: F ratio
@@ -68,11 +70,51 @@ class Anova(object):
         between_ssq = np.sum(self.count_ser*self.mean_ser**2)  \
               - np.sum(self.value_ser**2)/self.num_tot
         within_df = self.num_tot - self.num_level
-        stat = between_ssq/within_ssq
         between_df = self.num_level - 1
-        sl = 1 - f.cdf(stat, within_df, between_df)
+        if (np.isclose(within_ssq, 0)) or (between_df == 0):
+            sl = 1
+            stat = np.nan
+        else:
+            stat = between_ssq/within_ssq
+            sl = 1 - f.cdf(stat, within_df, between_df)
         return FStatistic(stat=stat, sl=sl, wdf=within_df, bdf=between_df)
 
-        
-        
- 
+    @classmethod
+    def calcSl(cls, instance_names, df, factor_name, replication_name, value_name):
+        """
+        Calculates significance levels for all distinct alues of instance_names.
+
+        Parameters
+        ----------
+        instance_names: list-str (columns that define instances)
+        factor_name: str (name of factor whose levels are compared)
+        replication_name: str (column that identifies replications of the factor level)
+        value_column: str (value compared)
+        df: pd.DataFrame
+            index: instances
+            columns: factor_name, replication_name
+
+        Returns
+        -------
+        pd.Series
+            index: values of instance_names
+            value: significance levels
+        """
+        # Construct the instance column
+        instances = []
+        for _, row in df.iterrows():
+            instance = SEPARATOR.join([str(i) for i in row[instance_names].values])
+            instances.append(instance)
+        new_df = df.copy()
+        new_df[INSTANCE] = instances
+        # Construct the significance levels
+        sls = []
+        instance_idxs = list(set(instances))
+        for instance in instance_idxs:
+            t_df = new_df[new_df[INSTANCE] == instance]
+            anova = Anova(t_df, factor_name, replication_name, value_name)
+            sls.append(anova.fstat.sl)
+        ser = pd.Series(sls, index=instance_idxs)
+        return ser
+            
+
--- tests/analysis/test_anova.py
+++ tests/analysis/test_anova.py
@@ -1,4 +1,4 @@
-from smarte.analysis.anova import Anova
+from smarte.analysis import anova as anv
 import smarte.constants as cn
 
 import os
@@ -11,20 +11,20 @@ IGNORE_TEST = False
 IS_PLOT = False
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_FILE = os.path.join(TEST_DIR, "test_anova.csv")
-DF = pd.read_csv(TEST_FILE)
+DF_FULL = pd.read_csv(TEST_FILE)
 CAT_NUM_SPECIES = "cat_num_species"
 values = []
-for value in DF[cn.SD_NUM_SPECIES]:
+for value in DF_FULL[cn.SD_NUM_SPECIES]:
     if (value is None) or (value <= 0) or np.isnan(value):
         new_value = 0
     else:
         new_value = int(np.log10(value))
     values.append(new_value)
-DF[CAT_NUM_SPECIES] = values
+DF_FULL[CAT_NUM_SPECIES] = values
 FACTOR_NAME = CAT_NUM_SPECIES
 REPLICATION_NAME = cn.SD_TS_INSTANCE
 VALUE_NAME = cn.SD_MEDIAN_ERR
-DF = DF[[FACTOR_NAME, REPLICATION_NAME, VALUE_NAME]]
+DF = DF_FULL[[FACTOR_NAME, REPLICATION_NAME, VALUE_NAME]]
         
 
 #############################
@@ -33,11 +33,30 @@ DF = DF[[FACTOR_NAME, REPLICATION_NAME, VALUE_NAME]]
 class TestAnova(unittest.TestCase):
 
     def setUp(self):
-        self.anova = Anova(DF, FACTOR_NAME, REPLICATION_NAME, VALUE_NAME)
+        self.anova = anv.Anova(DF, FACTOR_NAME, REPLICATION_NAME, VALUE_NAME)
 
     def testContructor(self):
         if IGNORE_TEST:
             return
+        self.assertGreater(self.anova.fstat.sl, 0)
+
+    def testCalcSl1(self):
+        if IGNORE_TEST:
+            return
+        sl_ser = anv.Anova.calcSl([cn.SD_MAX_FEV, cn.SD_METHOD], DF_FULL, FACTOR_NAME,
+              REPLICATION_NAME, VALUE_NAME)
+        self.assertEqual(len(sl_ser), 2)
+
+    def testCalcSl2(self):
+        if IGNORE_TEST:
+            return
+        instance_names = [cn.SD_BIOMODEL_NUM, cn.SD_MAX_FEV, cn.SD_METHOD]
+        sl_ser = anv.Anova.calcSl(instance_names, 
+              DF_FULL, FACTOR_NAME, REPLICATION_NAME, VALUE_NAME)
+        indices = list(sl_ser.index)
+        self.assertEqual(indices[0].count(anv.SEPARATOR), len(instance_names) - 1)
+        ser = sl_ser[sl_ser < 1.0]
+        self.assertEqual(len(ser), 1)
 
 
 if __name__ == '__main__':
